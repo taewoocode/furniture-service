d29af9fb1fae5365c0cb4d1f26acf32dcb84a0fb
taewoocode <qkrxodn6035@gmail.com>
2025-05-27 18:31:41 +0900

    first commit

---
+++ src/test/kotlin/com/example/settle_furniture_service/SettleFurnitureServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.settle_furniture_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SettleFurnitureServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
